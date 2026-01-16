4d47efa1adaec77115d1d97bd970e5dc474a3ac0
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2026-01-16 16:04:18 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
     }
 }
 
