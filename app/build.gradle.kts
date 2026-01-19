992b7e1a292bb0c3af330aa3c5be8df4c2fdfe06
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2026-01-19 21:13:54 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.wannaphong.hostai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
