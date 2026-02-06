f7165b5501cdda336744f398e309cd33eeea5a7b
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2026-02-06 17:59:33 +0700

    Bump version name to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 4
-        versionName = "1.0.3"
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
