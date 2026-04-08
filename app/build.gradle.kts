d97ada554c0df9e4b277962bebdc92c73a600211
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2026-04-08 22:34:56 +0700

    Bump version code to 14 and version name to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.wannaphong.hostai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.1.2"
+        versionCode = 14
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
